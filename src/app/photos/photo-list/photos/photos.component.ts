852aa554e4ac06a24ea22802939d85e86ef15de9
Matheus Campos <theuscampos45@gmail.com>
2024-01-31 10:44:47 -0300

    Adicionando comentários no codigo photos component

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -7,25 +7,27 @@ import { Photo } from '../../photo/photo';
   styleUrl: './photos.component.css'
 })
 export class PhotosComponent implements OnChanges {
-  
+
   @Input() photos: Photo[] = [];
 
   rows: any[] = [];
 
   constructor() { }
+
+  // Detecta alteração em Photos[] e se houver agrupa de 3 em 3 elementos
   ngOnChanges(changes: SimpleChanges) {
-    if(changes['photos']) {
+    if (changes['photos']) {
       this.rows = this.groupColums(this.photos);
     }
   }
 
+  // itera no array em photos agrupando de 3 em 3 e retorna o novo array
+  // Ex.: Entra -> [1, 2, 3, 4, 5, 6] - Retorna -> [[1, 2, 3], [4, 5, 6]]
   groupColums(photos: Photo[]) {
     const newRows = [];
-
-    for(let i = 0; i < photos.length; i+=3) {
+    for (let i = 0; i < photos.length; i += 3) {
       newRows.push(photos.slice(i, i + 3));
     }
-
     return newRows;
   }
 
