5f7119794d2783ed55b9cc149be617b1fdcd71e2
Matheus Campos <theuscampos45@gmail.com>
2024-01-31 10:41:06 -0300

    Adicionando comentários no codigo filter-by-description

--- src/app/photos/photo-list/filter-by-description.pipe.ts
+++ src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -1,18 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Photo } from '../photo/photo';
 
+// Este é um filtro personalizado em Angular para filtrar uma lista de fotos com base na descrição.
 @Pipe({ name: 'filterByDescription' })
 export class FilterByDescriptionPipe implements PipeTransform {
-
+  
+  // Recebe um array de fotos e uma string de consulta para a descrição.
   transform(photos: Photo[], descriptionQuery: string) {
+
+    // Remove espaços em branco e converte a consulta para minúsculas para comparação.
     descriptionQuery = descriptionQuery.trim().toLowerCase();
 
-    if (descriptionQuery) {
-      return photos.filter(photo => 
+    // Filtra as fotos cuja descrição inclui a consulta, retornando o resultado.
+    // Se a consulta estiver vazia, retorna a lista de fotos inalterada.
+    if (descriptionQuery)  photos.filter(photo => 
         photo.description.toLowerCase().includes(descriptionQuery))
-    } else {
-      return photos;
-    }
+    return photos;
   }
-
 }
