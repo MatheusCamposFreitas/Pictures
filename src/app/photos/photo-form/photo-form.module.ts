98e58832bf1b438cb476569e94c6c762a5604825
Matheus Campos <theuscampos45@gmail.com>
2024-02-07 14:48:58 -0300

    Modificações no layout com bootstrap

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoFormComponent } from './photo-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VmessageModule } from '../../shared/components/vmessage/vmessage.module';
 
 
 
 @NgModule({
   declarations: [PhotoFormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    VmessageModule,
+    FormsModule
   ]
 })
 export class PhotoFormModule { }
