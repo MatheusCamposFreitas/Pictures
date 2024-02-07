dce9e9848b7e0c6d8d24b04d67d1019ca46cea8b
Matheus Campos <theuscampos45@gmail.com>
2024-02-07 15:06:08 -0300

    Adicionando validadores no photo-form

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-photo-form',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class PhotoFormComponent {
 
+  photoForm!: FormGroup;
+  
+  constructor(private formBuilder: FormBuilder) {
+    this.photoForm = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(255)],
+      allowComments: [true]
+    });
+  }
 }
